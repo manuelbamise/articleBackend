92b0fb1761b816dd331050025cb71ce62f14c5fe
Emmanuel Oluwabamise Tobi <102730033+manuelbamise@users.noreply.github.com>
2024-05-10 18:55:43 +0100

    Added logger

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{
+    logger: ['warn','error'],
+  });
+
+  
 
   const config = new DocumentBuilder()
   .setTitle('Article API')
